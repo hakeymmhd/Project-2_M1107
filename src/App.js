b13b33f8b44ca7e2eb7243b240d86da0821f8677
mhdhakeym <mhdhakeym786@gmail.com>
2021-11-07 22:11:14 +0800

    testing Spoonacular API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Main />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
