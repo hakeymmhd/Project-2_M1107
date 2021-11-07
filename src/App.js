1fac9cdb93246edc63ecf49776e434c4c56f08e5
mhdhakeym <mhdhakeym786@gmail.com>
2021-11-07 22:36:32 +0800

    tested card UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
+import RecipeReviewCard from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Main />
+        <RecipeReviewCard />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
