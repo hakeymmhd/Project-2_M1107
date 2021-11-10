945548fd43fd4efab245478bb432d71a5f576f7a
mhdhakeym <mhdhakeym786@gmail.com>
2021-11-10 21:36:30 +0800

    created keywords file

---
+++ src/API/Keywords.js
@@ -0,0 +1,24 @@
+const keywords = {
+    spices: ["red chili flakes", "black peppercorns", "coriander", 
+            "fennel seeds", "paprika", "oregano", "turmeric", 
+            "nutmeg", "bay leaves", "cayenne pepper", "thyme"],
+
+    pantry: ["bread crumbs", "pasta", "couscous", "rice", 
+            "all-purpose flour", "white sugar", "brown sugar", 
+            "baking powder", "yeast", "chicken stock", "beef stock"],
+
+    refrigerator: ["butter", "heavy cream", "eggs", "parmesan", 
+                "bacon", "parsley", "celery", "carrots", 
+                "lemons", "limes", "orange juice", "milk"],
+
+    condiments: ["ketchup", "mayonnaise", "extra virgin olive oil", 
+                "vegetable oil", "vinegar", "mustard", "honey"],
+
+    vegetables: ["garlic", "shallots", "potatoes", "yellow onions", 
+                "red onions", "tomatoes"],
+
+    cannedGoods: ["diced tomatoes", "tomato sauce", "tomato paste", 
+                 "crushed tomatoes", "beans"],
+}
+
+export default keywords;
