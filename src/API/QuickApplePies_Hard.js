66c81b71b0b5429b04c5f4cc22dc662fe67e1c44
mhdhakeym <mhdhakeym786@gmail.com>
2021-11-16 22:33:02 +0800

    rendering detailed recipe instructions

---
+++ src/API/QuickApplePies_Hard.js
@@ -0,0 +1,335 @@
+const quickApplePies = {
+    "vegetarian": true,
+    "vegan": false,
+    "glutenFree": false,
+    "dairyFree": true,
+    "veryHealthy": false,
+    "cheap": false,
+    "veryPopular": true,
+    "sustainable": false,
+    "weightWatcherSmartPoints": 8,
+    "gaps": "no",
+    "lowFodmap": false,
+    "preparationMinutes": 10,
+    "cookingMinutes": 15,
+    "aggregateLikes": 1005,
+    "spoonacularScore": 25.0,
+    "healthScore": 1.0,
+    "creditsText": "Moms Dish",
+    "sourceName": "Moms Dish",
+    "pricePerServing": 62.6,
+    "extendedIngredients": [
+        {
+            "id": 9003,
+            "aisle": "Produce",
+            "image": "apple.jpg",
+            "consistency": "solid",
+            "name": "apples",
+            "nameClean": "apple",
+            "original": "5 medium Apples",
+            "originalString": "5 medium Apples",
+            "originalName": "Apples",
+            "amount": 5.0,
+            "unit": "medium",
+            "meta": [],
+            "metaInformation": [],
+            "measures": {
+                "us": {
+                    "amount": 5.0,
+                    "unitShort": "medium",
+                    "unitLong": "mediums"
+                },
+                "metric": {
+                    "amount": 5.0,
+                    "unitShort": "medium",
+                    "unitLong": "mediums"
+                }
+            }
+        },
+        {
+            "id": 1123,
+            "aisle": "Milk, Eggs, Other Dairy",
+            "image": "egg.png",
+            "consistency": "solid",
+            "name": "egg",
+            "nameClean": "egg",
+            "original": "1 Egg",
+            "originalString": "1 Egg",
+            "originalName": "Egg",
+            "amount": 1.0,
+            "unit": "",
+            "meta": [],
+            "metaInformation": [],
+            "measures": {
+                "us": {
+                    "amount": 1.0,
+                    "unitShort": "",
+                    "unitLong": ""
+                },
+                "metric": {
+                    "amount": 1.0,
+                    "unitShort": "",
+                    "unitLong": ""
+                }
+            }
+        },
+        {
+            "id": 20081,
+            "aisle": "Baking",
+            "image": "flour.png",
+            "consistency": "solid",
+            "name": "flour",
+            "nameClean": "wheat flour",
+            "original": "9 teaspoons Flour",
+            "originalString": "9 teaspoons Flour",
+            "originalName": "Flour",
+            "amount": 9.0,
+            "unit": "teaspoons",
+            "meta": [],
+            "metaInformation": [],
+            "measures": {
+                "us": {
+                    "amount": 9.0,
+                    "unitShort": "tsps",
+                    "unitLong": "teaspoons"
+                },
+                "metric": {
+                    "amount": 9.0,
+                    "unitShort": "tsps",
+                    "unitLong": "teaspoons"
+                }
+            }
+        },
+        {
+            "id": 10018338,
+            "aisle": "Refrigerated;Frozen",
+            "image": "dough.jpg",
+            "consistency": "solid",
+            "name": "pastry dough",
+            "nameClean": "shortcrust pastry",
+            "original": "1 sheet Pastry Dough",
+            "originalString": "1 sheet Pastry Dough",
+            "originalName": "Pastry Dough",
+            "amount": 1.0,
+            "unit": "sheet",
+            "meta": [],
+            "metaInformation": [],
+            "measures": {
+                "us": {
+                    "amount": 1.0,
+                    "unitShort": "sheet",
+                    "unitLong": "sheet"
+                },
+                "metric": {
+                    "amount": 1.0,
+                    "unitShort": "sheet",
+                    "unitLong": "sheet"
+                }
+            }
+        },
+        {
+            "id": 19335,
+            "aisle": "Baking",
+            "image": "sugar-in-bowl.png",
+            "consistency": "solid",
+            "name": "sugar",
+            "nameClean": "sugar",
+            "original": "1 cup Sugar",
+            "originalString": "1 cup Sugar",
+            "originalName": "Sugar",
+            "amount": 1.0,
+            "unit": "cup",
+            "meta": [],
+            "metaInformation": [],
+            "measures": {
+                "us": {
+                    "amount": 1.0,
+                    "unitShort": "cup",
+                    "unitLong": "cup"
+                },
+                "metric": {
+                    "amount": 236.588,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        }
+    ],
+    "id": 618390,
+    "title": "Quick Apple Pies",
+    "readyInMinutes": 25,
+    "servings": 6,
+    "sourceUrl": "http://momsdish.com/recipe/536/quick-apple-pies",
+    "image": "https://spoonacular.com/recipeImages/618390-556x370.jpg",
+    "imageType": "jpg",
+    "summary": "Quick Apple Pies might be just the dessert you are searching for. This dairy free and vegetarian recipe serves 6 and costs <b>63 cents per serving</b>. One serving contains <b>239 calories</b>, <b>2g of protein</b>, and <b>1g of fat</b>. 1005 people have made this recipe and would make it again. Head to the store and pick up apples, pastry dough, sugar, and a few other things to make it today. From preparation to the plate, this recipe takes around <b>25 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 26%</b>. This score is not so great. Try <a href=\"https://spoonacular.com/recipes/quick-chicken-pot-pies-for-two-170670\">Quick Chicken Pot Pies for Two</a>, <a href=\"https://spoonacular.com/recipes/quick-blueberry-hand-pies-530936\">(Quick) Blueberry Hand Pies</a>, and <a href=\"https://spoonacular.com/recipes/quick-and-easy-cheeseburger-pies-553811\">Quick and Easy Cheeseburger Pies</a> for similar recipes.",
+    "cuisines": [],
+    "dishTypes": [
+        "side dish"
+    ],
+    "diets": [
+        "dairy free",
+        "lacto ovo vegetarian"
+    ],
+    "occasions": [],
+    "winePairing": {},
+    "instructions": "Prepare ingredients for the pies. Try picking out hard, tart apples, but other apples would work just as great. Peel and slice apples into 8-10 pieces, set aside. Roll out the pastry dough on a floured surface. Cut the pastry sheet into 9 equal pieces. Into the middle of each piece, place 4-5 pieces of apples, a spoonful of sugar and a teaspoon of flour into each one. Fold dough over the apples, forming little pockets from it.Place pies on a lined baking sheet. Whisk egg in a separate bowl. Brush each pie with eggs, sprinkling them with some sugar.Bake pies at 415F for about 15 minutes or until golden brown.",
+    "analyzedInstructions": [
+        {
+            "name": "",
+            "steps": [
+                {
+                    "number": 1,
+                    "step": "Prepare ingredients for the pies. Try picking out hard, tart apples, but other apples would work just as great. Peel and slice apples into 8-10 pieces, set aside.",
+                    "ingredients": [
+                        {
+                            "id": 1029003,
+                            "name": "tart apple",
+                            "localizedName": "tart apple",
+                            "image": "grannysmith-apple.png"
+                        },
+                        {
+                            "id": 9003,
+                            "name": "apple",
+                            "localizedName": "apple",
+                            "image": "apple.jpg"
+                        }
+                    ],
+                    "equipment": []
+                },
+                {
+                    "number": 2,
+                    "step": "Roll out the pastry dough on a floured surface.",
+                    "ingredients": [
+                        {
+                            "id": 10018338,
+                            "name": "pastry dough",
+                            "localizedName": "pastry dough",
+                            "image": "dough.jpg"
+                        },
+                        {
+                            "id": 0,
+                            "name": "roll",
+                            "localizedName": "roll",
+                            "image": "dinner-yeast-rolls.jpg"
+                        }
+                    ],
+                    "equipment": []
+                },
+                {
+                    "number": 3,
+                    "step": "Cut the pastry sheet into 9 equal pieces. Into the middle of each piece, place 4-5 pieces of apples, a spoonful of sugar and a teaspoon of flour into each one. Fold dough over the apples, forming little pockets from it.",
+                    "ingredients": [
+                        {
+                            "id": 9003,
+                            "name": "apple",
+                            "localizedName": "apple",
+                            "image": "apple.jpg"
+                        },
+                        {
+                            "id": 0,
+                            "name": "dough",
+                            "localizedName": "dough",
+                            "image": "pizza-dough"
+                        },
+                        {
+                            "id": 20081,
+                            "name": "all purpose flour",
+                            "localizedName": "all purpose flour",
+                            "image": "flour.png"
+                        },
+                        {
+                            "id": 19335,
+                            "name": "sugar",
+                            "localizedName": "sugar",
+                            "image": "sugar-in-bowl.png"
+                        }
+                    ],
+                    "equipment": []
+                },
+                {
+                    "number": 4,
+                    "step": "Place pies on a lined baking sheet.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 404727,
+                            "name": "baking sheet",
+                            "localizedName": "baking sheet",
+                            "image": "baking-sheet.jpg"
+                        }
+                    ]
+                },
+                {
+                    "number": 5,
+                    "step": "Whisk egg in a separate bowl.",
+                    "ingredients": [
+                        {
+                            "id": 1123,
+                            "name": "egg",
+                            "localizedName": "egg",
+                            "image": "egg.png"
+                        }
+                    ],
+                    "equipment": [
+                        {
+                            "id": 404661,
+                            "name": "whisk",
+                            "localizedName": "whisk",
+                            "image": "whisk.png"
+                        },
+                        {
+                            "id": 404783,
+                            "name": "bowl",
+                            "localizedName": "bowl",
+                            "image": "bowl.jpg"
+                        }
+                    ]
+                },
+                {
+                    "number": 6,
+                    "step": "Brush each pie with eggs, sprinkling them with some sugar.",
+                    "ingredients": [
+                        {
+                            "id": 19335,
+                            "name": "sugar",
+                            "localizedName": "sugar",
+                            "image": "sugar-in-bowl.png"
+                        },
+                        {
+                            "id": 1123,
+                            "name": "egg",
+                            "localizedName": "egg",
+                            "image": "egg.png"
+                        }
+                    ],
+                    "equipment": []
+                },
+                {
+                    "number": 7,
+                    "step": "Bake pies at 415F for about 15 minutes or until golden brown.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 404784,
+                            "name": "oven",
+                            "localizedName": "oven",
+                            "image": "oven.jpg",
+                            "temperature": {
+                                "number": 415.0,
+                                "unit": "Fahrenheit"
+                            }
+                        }
+                    ],
+                    "length": {
+                        "number": 15,
+                        "unit": "minutes"
+                    }
+                }
+            ]
+        }
+    ],
+    "originalId": null
+}
+
+export default quickApplePies;
--- src/navigation/result-detailed.jsx
+++ src/navigation/result-detailed.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import recipesById from "../API/RecipesById_Hard";
+import quickApplePies from "../API/QuickApplePies_Hard";
 
 export default function ResultDetailed() {
-    const RenderIngredients = () => {
-        return (<p>{recipesById[0].extendedIngredients[0].original}</p>);
+    
+   
+    function RenderIngredients() {
+        let ingredientString = "";
+        quickApplePies.extendedIngredients.map((element)=> {
+            return element.name
+        });
+      
+        return ingredientString;
+    }
+    // console.log(ingredientString)
+    const RenderRecipe = () => {
+        return (
+            <div>
+                <img src={quickApplePies.image} />
+                <p>{`Title: ${quickApplePies.title}`}</p>
+                <p>{`Cooking time: ${quickApplePies.readyInMinutes}`}</p>
+                <p>{`Serves: ${quickApplePies.servings}`}</p>
+                <p>{`Ingredients: `} </p>
+                <RenderIngredients />
+                
+                <section>{RenderIngredients}</section>
+            </div>
+        
+        );
     }
 
     return (
         <>
             <p>Details of results!</p>
-            <RenderIngredients />
+            <RenderRecipe />
+            
         </>
         
     )
